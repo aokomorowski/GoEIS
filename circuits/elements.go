4482fc33f58bed9ac9cec0962fc656b86ce403cc
Artur Komorowski <artur.komorowski@myaic.com>
2020-03-22 23:52:36 +0100

    add elements' functions and their tests

---
+++ circuits/elements.go
@@ -0,0 +1,21 @@
+// TODO: implement R, C, L, W, W_0, W_e, CPE
+package circuits
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func CreateResistor(resistance float64) (float64, error) {
+	var err error
+	if resistance < 0 {
+		err = fmt.Errorf("Given resistance is lesser than 0 : %g", resistance)
+		log.Error(err)
+	}
+	return resistance, err
+}
+
+// func CreateCapacitor(conductivity float64) (float64 error) {
+// 	return impedance, err
+// }
